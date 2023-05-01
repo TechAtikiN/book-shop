e564e016ac7b62d4335bf15753e39c8cd961c83b
techatikin <nikitakhabya03@gmail.com>
2023-05-01 13:18:10 +0530

    feat: CRUD implementation for a books collection in mongodb

--- routes/books.js
+++ routes/books.js
@@ -1,25 +1,81 @@
 const express = require('express')
 const router = express.Router()
 
-const Post = require('../models/Book')
+const Book = require('../models/Book')
 
-router.get('/', (req, res) => {
-  res.send('Gives all books')
+router.get('/', async (req, res) => {
+
+  try {
+    const books = await Book.find()
+    res.json(books)
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
-router.get('/:bookId', (req, res) => {
+router.get('/:bookId', async (req, res) => {
+
   const bookId = req.params.bookId
 
-  res.status(200).json({
-    message: `Book with ID of ${bookId}`
+  try {
+    const book = await Book.findById(bookId)
+    res.json(book)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+router.post('/', async (req, res) => {
+
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    pages: req.body.pages
+  })
+
+  try {
+    const savedBook = await book.save()
+    res.json(savedBook)
+  } catch (err) {
+    res.json({ message: err })
+  }
+
+  book.save().then((data) => {
+    res.json(data)
+  }).catch((err) => {
+    res.json({ message: err })
   })
 })
 
+router.patch('/:bookId', async (req, res) => {
 
-router.post('/', (req, res) => {
-  res.status(201).json({
-    message: `Created new book`
-  })
+  const bookId = req.params.bookId
+  try {
+    const updatedBook = await Book.updateOne(
+      { _id: bookId },
+      {
+        $set: {
+          title: req.body.title,
+          author: req.body.author,
+          pages: req.body.pages
+        }
+      },
+    )
+    res.json(updatedBook)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+router.delete('/:bookId', async (req, res) => {
+
+  const bookId = req.params.bookId
+  try {
+    const removePost = await Book.deleteOne({ _id: bookId })
+    res.json(removePost)
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 module.exports = router
